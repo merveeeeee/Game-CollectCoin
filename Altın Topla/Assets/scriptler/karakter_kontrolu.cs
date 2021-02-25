78e548634660b7b9fe851cabd519fa070abfe3bc
Merve Nur <71434225+merveeeeee@users.noreply.github.com>
2021-02-25 10:09:19 +0300

    Deneme Projesi Ekle

---
+++ Altın Topla/Assets/scriptler/karakter_kontrolu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class karakter_kontrolu : MonoBehaviour {
+
+	// Use this for initialization
+	public float hiz ;
+	public Animator animasyoncum;
+	public int altin_miktari;
+	public Text altin_miktari_text;
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void FixedUpdate(){
+		float horizontal = Input.GetAxis ("Horizontal");
+		transform.position += new Vector3 (horizontal*hiz*Time.deltaTime, 0, 0);
+
+		bool kosuyorum = false;
+		if (horizontal != 0) {
+			kosuyorum = true;
+		}
+		animasyoncum.SetBool ("kosuyor_muyum", kosuyorum);
+
+		if (horizontal > 0) {
+			transform.localScale = new Vector3(1,1,1);
+		}
+		else if (horizontal<0){
+			transform.localScale = new Vector3(-1,1,1);
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D carpilan_nesne){
+		if (carpilan_nesne.tag == "altinlar"){
+			altin_miktari ++ ;
+			altin_miktari_text.text = altin_miktari.ToString();
+			Destroy (carpilan_nesne.gameObject);
+
+		}
+	}
+
+	void LateUpdate(){
+
+	}
+}
---
+++ Altın Topla/Assets/scriptler/yerde_mi.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yerde_mi : MonoBehaviour {
+	public LayerMask layer; 
+	public bool yerde_miyim;
+	public Rigidbody2D rigitbod;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit2D carpisma = Physics2D.Raycast (transform.position, Vector2.down, 0.1f, layer);
+		if (carpisma.collider != null) {
+			yerde_miyim = true;
+		} else {
+			yerde_miyim = false;
+		}
+		if (Input.GetKeyDown (KeyCode.Space) && yerde_miyim == true) {
+			rigitbod.velocity += new Vector2 (0, 5F);
+		} 
+		else if (Input.GetKeyDown (KeyCode.W) && yerde_miyim == true) {
+			rigitbod.velocity += new Vector2 (0, 5F);
+		} 
+		else if (Input.GetKeyDown (KeyCode.UpArrow) && yerde_miyim == true) {
+			rigitbod.velocity += new Vector2 (0, 5F);
+		}
+	}
+}
